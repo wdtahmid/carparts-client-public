2d6ad36f760f5ca5966a4c862ef95257428e100c
Tahmid Hasan <wdtahmid@gmail.com>
2022-05-22 00:06:18 +0600

    SetUp daisyUi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#9e0109",
+
+          "secondary": "#006643",
+
+          "accent": "#ea0e80",
+
+          "neutral": "#1F2028",
+
+          "base-100": "#373B3E",
+
+          "info": "#6BB2EF",
+
+          "success": "#21DE7C",
+
+          "warning": "#926107",
+
+          "error": "#FC534A",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
