986a3ce8304dbcccf53c2a5ff2585be64d3b363e
Tahmid Hasan <wdtahmid@gmail.com>
2022-05-22 00:43:20 +0600

    Add modal for login form

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -25,7 +25,14 @@ const Header = () => {
             </div>
             <div className="navbar-end">
                 <label htmlFor="my-modal-3" className="btn modal-button"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg> open modal</label>
-
+                <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box relative">
+                        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                        <h3 className="text-lg font-bold">Congratulations random Interner user!</h3>
+                        <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
