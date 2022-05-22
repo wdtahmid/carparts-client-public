35eb3cbc51e4fc480d67d10c07b5001c186645f9
Tahmid Hasan <wdtahmid@gmail.com>
2022-05-23 05:49:38 +0600

    Successfully worked with add review

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../hookes/firebase.init';
 
 const AddReview = () => {
@@ -24,47 +25,49 @@ const AddReview = () => {
             body: JSON.stringify(userReview)
         })
             .then(res => {
-                console.log(res);
                 return res.json();
             })
             .then(data => {
-                console.log(data);
+                if (data.acknowledged === true) {
+                    toast.success('Review added successfully!');
+                    e.target.reset();
+                }
             })
     }
     return (
         <div>
             <h2 className='text-3xl text-primary mt-3 uppercase font-semibold mb-4'>Add A Review</h2>
             <form onSubmit={addUserReview} className='p-5'>
-                <textarea name='review' class="textarea rounded-sm textarea-primary w-full" rows={6} placeholder="Write your review here"></textarea>
+                <textarea name='review' className="textarea rounded-sm textarea-primary w-full" rows={6} placeholder="Write your review here"></textarea>
                 <div className='flex flex-wrap gap-4'>
-                    <div class="form-control w-[100px]">
-                        <label class="label cursor-pointer">
-                            <span class="label-text">One Star</span>
-                            <input value="1" type="radio" name="radio" class="radio checked:bg-red-500" checked />
+                    <div className="form-control w-[100px]">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">One Star</span>
+                            <input value="1" type="radio" name="radio" className="radio checked:bg-red-500" />
                         </label>
                     </div>
-                    <div class="form-control w-[100px]">
-                        <label class="label cursor-pointer">
-                            <span class="label-text">Two Star</span>
-                            <input value="2" type="radio" name="radio" class="radio checked:bg-blue-500" checked />
+                    <div className="form-control w-[100px]">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Two Star</span>
+                            <input value="2" type="radio" name="radio" className="radio checked:bg-blue-500" />
                         </label>
                     </div>
-                    <div class="form-control w-[100px]">
-                        <label class="label cursor-pointer">
-                            <span class="label-text">Three Star</span>
-                            <input value="3" type="radio" name="radio" class="radio checked:bg-blue-900" checked />
+                    <div className="form-control w-[100px]">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Three Star</span>
+                            <input value="3" type="radio" name="radio" className="radio checked:bg-blue-900" />
                         </label>
                     </div>
-                    <div class="form-control w-[100px]">
-                        <label class="label cursor-pointer">
-                            <span class="label-text">Four Star</span>
-                            <input value="4" type="radio" name="radio" class="radio checked:bg-primary" checked />
+                    <div className="form-control w-[100px]">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Four Star</span>
+                            <input value="4" type="radio" name="radio" className="radio checked:bg-primary" />
                         </label>
                     </div>
-                    <div class="form-control w-[100px]">
-                        <label class="label cursor-pointer">
-                            <span class="label-text">Five Star</span>
-                            <input value="5" type="radio" name="radio" class="radio checked:bg-info" checked />
+                    <div className="form-control w-[100px]">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Five Star</span>
+                            <input value="5" type="radio" name="radio" className="radio checked:bg-info" defaultChecked />
                         </label>
                     </div>
                 </div>
