879530154eee007b02a89fc772437ff161560bd0
Tahmid Hasan <wdtahmid@gmail.com>
2022-05-26 06:48:10 +0600

    add contact us section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import AboutUs from './Sections/AboutUs/AboutUs';
+import ContactUsSection from './Sections/Contact/ContactUsSection';
 import Parts from './Sections/Parts/Parts';
 import Reviews from './Sections/Reviews/Reviews';
 import Summery from './Sections/Summery/Summery';
@@ -13,6 +14,7 @@ const Home = () => {
             <Summery />
             <Reviews />
             <AboutUs />
+            <ContactUsSection />
         </div>
     );
 };
